2c6461acf106676e40827e971e914111dcb2af31
balboaid <balboanwafor@gmail.com>
2026-02-01 05:48:58 -0300

    fix: corrigindo export default na Home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold">Página Home Temporall</h1>
+      {/* Resto do seu código aqui */}
+    </div>
+  );
+}
